f1af4ff3e846431975d084067f6ccdd20ba72646
ZuchiSpeed <zuchi737@gmail.com>
2025-09-29 17:12:53 +0200

    main layout & sidebar UI

---
+++ components/mobile-header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MobileSidebar } from "./mobile-sidebar";
+
+export const MobileHeader = () => {
+  return (
+    <nav className="lg:hidden px-6 flex items-center border-b fixed top-0 w-full z-50 h-[50px] bg-green-500">
+      <MobileSidebar />
+    </nav>
+  );
+};
